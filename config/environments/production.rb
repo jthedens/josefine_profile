0f2a7164990af60e9ec2fe54ae90368584b066db
Josefine Theden-Schow <josi.theden@gmx.de>
2020-02-20 11:59:58 +0100

    API Key added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+   api_key: 'ENV["mailgun_secret_api_key"]',
+   domain: 'mydomain.com',
+  }
 end
